0bd1993e6bfbfe65b4e4ebd6c9dbc7e22d1c6a98
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-03-23 20:28:48 +0900

    chore: some fix

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const { PORT = 3000 } = process.env as Env;
 const app = express();
 app.use(express.json());
 app.get('/', (_, res) => {
-  res.send({ message: 'hello world!' });
+  res.send({ message: 'hello world!!' });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
